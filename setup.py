7dfe36c047c1ea5a9bf64d76e434dad80b465ec9
RobinDavid <dev.robin.david@gmail.com>
2018-10-30 23:49:19 +0100

    make a setup.py script

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup
+
+setup(
+    name='python-binexport',
+    version='0.1',
+    description='Python wrapper to manipulate binexport files (protobuf)',
+    author='Robin David',
+    author_email='rdavid@quarkslab.com',
+    url='https://gitlab.qb/rdavid/python-binexport',
+    packages=['binexport'],
+    install_requires=[
+        'python-magic',
+        'click',
+        'protobuf',
+        'networkx'
+        # idascript to generate .BinExport files
+    ],
+    scripts=['bin/binexporter']
+)
