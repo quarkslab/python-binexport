f538a4ba5f77f3abc6f90cd329337af958ccb5d0
rcohen <rcohen@quarkslab.com>
2023-03-22 17:07:19 +0000

    Add doc

--- binexport/instruction.py
+++ binexport/instruction.py
@@ -1,11 +1,12 @@
 import weakref
 
 from binexport.operand import OperandBinExport
+from typing import List
 
 
 class InstructionBinExport:
     """
-    Instruction class. It represent an instruction with its operands.
+    Instruction class. It represents an instruction with its operands.
     """
 
     def __init__(
@@ -17,6 +18,7 @@ class InstructionBinExport:
     ):
         """
         Instruction constructor.
+
         :param program: Weak reference to the program
         :param function: Weak reference to the function
         :param addr: address of the instruction (computed outside)
@@ -45,29 +47,39 @@ class InstructionBinExport:
 
     @property
     def program(self) -> "ProgramBinExport":
-        """Wrapper on weak reference on ProgramBinExport"""
+        """
+        Wrapper on weak reference on ProgramBinExport
+
+        :return: the object `ProgramBinExport` that represents the program associated to the instruction
+        """
+
         return self._program()
 
     @property
     def pb_instr(self) -> "BinExport2.Instruction":
         """
         Returns the Instruction object in the binexport protobuf
+
         :return: Instruction binexport object
         """
+
         return self.program.proto.instruction[self._idx]
 
     @property
     def mnemonic(self) -> str:
         """
         Returns the mnemonic string as gathered by binexport
+
         :return: mnemonic string (with prefix)
         """
+
         return self.program.proto.mnemonic[self.pb_instr.mnemonic_index].name
 
     @property
-    def operands(self) -> list[OperandBinExport]:
+    def operands(self) -> List[OperandBinExport]:
         """
         Returns a list of the operands instanciated dynamically on-demand.
+
         :return: list of operand objects
         """
         return [
