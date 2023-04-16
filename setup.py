2d2449449787ca753c1f09891883fdf4a94b6de0
RobinDavid <dev.robin.david@gmail.com>
2023-04-16 09:30:18 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,37 @@
 from setuptools import setup
 
+with open("README.md") as f:
+    README = f.read()
+
+
 setup(
     name="python-binexport",
-    version="0.1",
+    version="0.1.0",
     description="Python wrapper to manipulate binexport files (protobuf)",
+    long_description_content_type='text/markdown',
+    long_description=README,
     author="Robin David",
     author_email="rdavid@quarkslab.com",
-    url="https://gitlab.qb/rdavid/python-binexport",
+    url="https://github.com/quarkslab/python-binexport",
     packages=["binexport"],
+    project_urls={
+        "Documentation": "https://quarkslab.github.io/diffing-portal/exporter/binexport.html#python-binexport",
+        "Bug Tracker": "https://github.com/quarkslab/python-binexport/issues",
+        "Source": "https://github.com/quarkslab/python-binexport"
+    },
     install_requires=[
         "python-magic",
         "click",
         "protobuf",
         "networkx",
-        "enum_tools"
+        "enum_tools",
+        "idascript"
     ],
-    extras_require={"idascript": ["idascript"]},
     scripts=["bin/binexporter"],
+    license="AGPL-3.0",
+    classifiers=[
+        'Topic :: Security',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+    ],
 )
